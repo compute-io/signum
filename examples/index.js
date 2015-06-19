f96aeca1313d08e9b11fa21100542b74b90d2e59
Planeshifter <pburckhardt@outlook.com>
2015-06-19 10:02:49 +0200

    [UPDATE] README, examples, eqn

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,75 @@
 'use strict';
 
-var signum = require( './../lib' );
+var matrix = require( 'dstructs-matrix' ),
+	signum = require( './../lib' );
 
-var data = new Array( 100 );
-for ( var i = 0; i < data.length; i++ ) {
-	data[ i ] = Math.random() - 0.5;
+var data,
+	mat,
+	out,
+	tmp,
+	i;
+
+// ----
+// Plain arrays...
+data = new Array( 10 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.random()*20 - 10;
 }
+out = signum( data );
+console.log( 'Arrays: %s\n', out );
 
-console.log( signum( data ) );
+
+// ----
+// Object arrays (accessors)...
+function getValue( d ) {
+	return d.x;
+}
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': data[ i ]
+	};
+}
+out = signum( data, {
+	'accessor': getValue
+});
+console.log( 'Accessors: %s\n', out );
+
+
+// ----
+// Deep set arrays...
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': [ i, data[ i ].x ]
+	};
+}
+out = signum( data, {
+	'path': 'x/1',
+	'sep': '/'
+});
+console.log( 'Deepset:' );
+console.dir( out );
+console.log( '\n' );
+
+
+// ----
+// Typed arrays...
+data = new Int32Array( 10 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.random()*20 - 10;
+}
+tmp = signum( data );
+out = '';
+for ( i = 0; i < data.length; i++ ) {
+	out += tmp[ i ];
+	if ( i < data.length-1 ) {
+		out += ',';
+	}
+}
+console.log( 'Typed arrays: %s\n', out );
+
+
+// ----
+// Matrices...
+mat = matrix( data, [5,2], 'int32' );
+out = signum( mat );
+console.log( 'Matrix: %s\n', out.toString() );
